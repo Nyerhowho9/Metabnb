5f1727edc0b67409804656524838d4f9819bab3d
Nyerhowho9 <nyerhowho9@gmail.com>
2022-11-15 13:17:21 +0100

    Created skeleton for footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,17 @@ import { Component } from "react";
 
 class Footer extends Component{
     render(){
-        return <div></div>;
+        return <div className="footer">
+            <div className="row">
+                <div className="col-md-3 col-sm-6">1</div>
+                <div className="col-md-3 col-sm-6">2</div>
+                <div className="col-md-3 col-sm-6">3</div>
+                <div className="col-md-3 col-sm-6">4</div>
+            </div>
+            <div className="row">
+            <div className="col-12">trademark</div>
+            </div>
+        </div>;
     }
 }
 
