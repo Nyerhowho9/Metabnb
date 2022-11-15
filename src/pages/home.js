66bead34d9fbe35254705c3482613d47d8414e8c
Nyerhowho9 <nyerhowho9@gmail.com>
2022-11-15 13:10:47 +0100

    Built bottom.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,8 @@ import Header from "../components/header";
 import Hero from "../components/hero";
 import Brands from "../components/brands";
 import Main from "../components/main";
+import Bottom from "../components/bottom";
+import Footer from "../components/footer";
 
 function Home() {
     return <div className="home ">
@@ -10,6 +12,8 @@ function Home() {
         <Hero/>
         <Brands/>
         <Main/>
+        <Bottom/>
+        <Footer/>
 
     </div>;
   }
